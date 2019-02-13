c14a3908cc400aa853a5ef92fc389de724dac374
カキハナ_NKC <nkc.taisei.kakihana2016@gmail.com>
2019-02-13 10:56:05 +0900

    ランダムな座標に3秒間隔で生成するだけ。10秒で消える

---
+++ Assets/Kakihana/Scripts/PlanetDestroy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetDestroy : MonoBehaviour
+{
+
+    float time;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        if (time > 10.0f)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Kakihana/Scripts/PlanetSpawner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetSpawner : MonoBehaviour
+{
+    // 惑星自動生成スクリプト
+
+    [SerializeField] private int planetMaxnum;
+    [SerializeField] private int count;
+
+    public GameObject[] planetsPre; // 生成したい惑星
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("PlanetCreate", 3, 3);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void PlanetCreate()
+    {
+        if (count == planetMaxnum) return;
+        int spawnPlanetNum = Random.Range(0, planetsPre.Length);
+        float x = Random.Range(-20.0f, 20.0f);
+        float y = 0.0f;
+        float z = Random.Range(-20.0f, 20.0f);
+        Vector3 spawnPos = new Vector3(x, y, z);
+        Instantiate(planetsPre[spawnPlanetNum], spawnPos, Quaternion.identity);
+        count++;
+    }
+}
