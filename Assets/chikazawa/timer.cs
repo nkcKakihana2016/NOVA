1b203b7db7239661e6fe774215c49e863e27e85c
harurabbi <nkc.harutaka.chikazawa@gmail.com>
2019-02-15 12:22:41 +0900

    普通のタイマー実装

--- Assets/chikazawa/timer.cs
+++ Assets/chikazawa/timer.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UniRx;
 
 public class timer : MonoBehaviour
 {
@@ -23,11 +24,11 @@ public class timer : MonoBehaviour
         timeCount += Time.deltaTime;//毎フレームの時間を加算.
         int minute = (int)timeCount / 60;//分.timeを60で割った値.
         int second = (int)timeCount % 60;//秒.timeを60で割った余り.
-        int msecond = (int)(timeCount * 1000 % 100);
+        int msecond = (int)(timeCount * 100 % 99);
 
-
+        timerText.text = minute.ToString("00") + second.ToString("00") + msecond.ToString("00");
         //　マウスの左ボタン押しで一時停止
-        if ()
+        if (Input.GetMouseButtonDown(0))
         {
             //boolで処理 Mathf.Approximately( 比較, 比較) ? true(一致) : false;
             Time.timeScale = Mathf.Approximately(Time.timeScale, 0f) ? 1f : 0f;
