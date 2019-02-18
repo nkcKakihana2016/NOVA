096570de2a14f66113217843d0fbc03aaeb8cc32
カキハナ_NKC <nkc.taisei.kakihana2016@gmail.com>
2019-02-18 14:19:41 +0900

    スポーンシステム改良、シングルトンの導入

---
+++ Assets/Kakihana/Scripts/Test.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Test : MonoBehaviour
+{
+    // シーン遷移させるためだけに作ったもの。今後消します
+    float time;
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        if (time >= 10.0f)
+        {
+            SceneManager.LoadScene("Test2");
+        }
+    }
+}
